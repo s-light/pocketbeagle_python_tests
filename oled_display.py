af090e33f14083e67479790c4063a4337e739994
s-light <github@s-light.eu>
2019-04-02 11:21:25 +0200

    oled_idsplay.py more comments

--- oled_display.py
+++ oled_display.py
@@ -14,23 +14,24 @@ import busio
 
 import adafruit_ssd1306
 
+print("oled display tests")
+
+print("init spi")
 spi = busio.SPI(board.SCLK, board.MOSI)
+print("init digitalio")
 pin_cs = digitalio.DigitalInOut(board.P2_6)
 pin_rst = digitalio.DigitalInOut(board.P1_2)
 pin_dc = digitalio.DigitalInOut(board.P1_4)
 
+print("init SSD1306_SPI")
 # width, height, spi, dc, reset, cs
 display = adafruit_ssd1306.SSD1306_SPI(128, 32, spi, pin_dc, pin_rst, pin_cs)
 
-print("oled display tests")
-
-# Clear the display.  Always call show after changing pixels to make the display
-# update visible!
+print("clear display")
 display.fill(0)
 display.show()
 
-print("should be blank ;-)")
-
+print("draw some test pixels")
 # Set a pixel in the origin 0,0 position.
 display.pixel(0, 0, 1)
 # Set a pixel in the middle 64, 16 position.
