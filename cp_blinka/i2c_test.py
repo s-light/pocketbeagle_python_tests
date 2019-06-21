a27b77eb8cf77e8dc887f43cb3b2df4778c76fea
s-light <git@s-light.eu>
2019-06-21 23:46:10 +0200

    fix i2c_test

--- cp_blinka/i2c_test.py
+++ cp_blinka/i2c_test.py
@@ -5,7 +5,8 @@
 simple test for adafruit-circuitpython-apds9960.
 
     based on
-    https://learn.adafruit.com/adafruit-apds9960-breakout/circuitpython
+    https://circuitpython.readthedocs.io/en/latest/shared-bindings/busio/I2C.html#busio.I2C
+    https://circuitpython.readthedocs.io/projects/busdevice/en/latest/api.html#adafruit-bus-device-i2c-device-i2c-bus-device
 
 """
 
@@ -13,5 +14,5 @@ import board
 import busio
 
 print("basic i2c tests")
-with busio.I2C(SCL, SDA) as i2c:
-    print(i2c.scann())
+with busio.I2C(board.SCL, board.SDA) as i2c:
+    print(i2c.scan())
